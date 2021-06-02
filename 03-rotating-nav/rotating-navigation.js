e02f46655f26581191405b84369e111030b2dadd
Xiaolu Deng <sxiaolu86@gmail.com>
2021-06-02 13:56:32 -0600

    rotating-nav refinement

--- 03-rotating-nav/rotating-navigation.js
+++ 03-rotating-nav/rotating-navigation.js
@@ -2,6 +2,16 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', () => {
+  container.classList.add('show-nav')
+    open.style.display = "none";
+    close.style.display = "flex"
+})
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+close.addEventListener('click', () => {
+  container.classList.remove('show-nav')
+    close.style.display = "none";
+    open.style.display= "flex";
+})
+
